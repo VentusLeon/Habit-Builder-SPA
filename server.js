37a54cea13b240ed53fd7823b031122122645e35
VentusLeon <ryanholton1357@gmail.com>
2024-04-08 16:57:22 -0400

    got rid  of useless comment

--- server.js
+++ server.js
@@ -38,19 +38,6 @@ app.use(
 
 
 
-/*
-*                 Next thing to work on is to watch the client side portion of Art of Engineer video
-*                 and then figure out from that how to connect node.js backend to front end vue.js.
-*                 
-*
-*
-*                 ALSO need to implement testing after basic project structure is setup. 
-*                 Then, watch rest of Vue.js Course by freeCodeCamp and then implement final features.
-*
-*                 GOOD JOB ANOTHA GRIND AWAITS
-*/
-
-
 app.use("/", require("./routes/index"))
 app.use("/users", require("./routes/users"))
 
